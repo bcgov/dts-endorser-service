dcf0a9fe12a33b95c5173a05d9d4f9219787f621
Emiliano Suñé <emiliano.sune@gmail.com>
2024-12-10 17:22:05 -0800

    Removed backup-container configurations
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.sandbox.sh
+++ openshift/settings.sandbox.sh
@@ -10,4 +10,4 @@ SKIP_PIPELINE_PROCESSING=1
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export skip_git_overrides="aries-endorser-backup-build.yaml"
+export skip_git_overrides=""
